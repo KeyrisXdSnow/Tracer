ac8191e094eb08079ee8371e64bc87c15754f208
Keyris <KeyrisXdSnow>
2020-11-07 15:50:00 +0300

    add XMLSerializer

---
+++ TracerLib/Serialization/VXmlSerializer.cs
@@ -0,0 +1,31 @@
+﻿﻿using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using TracerLib.Tracer;
+using Formatting = System.Xml.Formatting;
+
+namespace TracerLib.Serialization
+{
+    public class VXmlSerializer : ISerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            var data = traceResult.GetThreadTraces().Values.ToArray();
+            var xmlSerializer = new XmlSerializer(data.GetType());
+            var stringWriter = new StringWriter();
+            
+            using (var writer = new XmlTextWriter(stringWriter))
+            {
+                writer.Formatting = Formatting.Indented;
+                xmlSerializer.Serialize(writer, data);
+            }
+
+            var result = stringWriter.ToString().Replace("ArrayOfThread", "root");
+
+            return result;
+            }
+        }
+    }
+    
+    
