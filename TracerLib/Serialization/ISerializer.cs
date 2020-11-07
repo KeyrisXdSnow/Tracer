99fbf2a6d22bcbd4591195575b414a1e5dacff67
Keyris <KeyrisXdSnow>
2020-11-07 15:49:49 +0300

    add JsonSerializer

---
+++ TracerLib/Serialization/ISerializer.cs
@@ -0,0 +1,9 @@
+﻿﻿using TracerLib.Tracer;
+
+namespace TracerLib.Serialization
+{
+    public interface ISerializer 
+    {
+        string Serialize(TraceResult traceResult);
+    }
+}
---
+++ TracerLib/Serialization/JsonSerializer.cs
@@ -0,0 +1,19 @@
+﻿﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using TracerLib.Tracer;
+
+namespace TracerLib.Serialization
+{
+    public class JsonSerializer : ISerializer
+    {
+        public string Serialize(TraceResult traceResult)
+        {
+            var arrays = new Dictionary<string, ICollection<ThreadTrace>>
+            {
+                {"threads", traceResult.GetThreadTraces().Values}
+            };
+            
+            return JsonConvert.SerializeObject(arrays, Formatting.Indented);
+        }
+    }
+}
